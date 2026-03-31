a6bf86044602fdd98e64a97ad045de243a3a294e
Jules-Rabus <jules200204@gmail.com>
2026-03-31 08:37:49 +0200

    fix(api): map User entity to UserMe DTO in CurrentUserProvider to avoid circular reference

--- api/src/State/User/CurrentUserProvider.php
+++ api/src/State/User/CurrentUserProvider.php
@@ -4,20 +4,27 @@ namespace App\State\User;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
+use App\Dto\User\UserMe;
+use App\Entity\User;
 use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\ObjectMapper\ObjectMapperInterface;
 
 /**
- * @implements ProviderInterface<UserInterface>
+ * @implements ProviderInterface<UserMe>
  */
 final readonly class CurrentUserProvider implements ProviderInterface
 {
-    public function __construct(private Security $security)
-    {
+    public function __construct(
+        private Security $security,
+        private ObjectMapperInterface $objectMapper,
+    ) {
     }
 
-    public function provide(Operation $operation, array $uriVariables = [], array $context = []): UserInterface
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): UserMe
     {
-        return $this->security->getUser();
+        /** @var User $user */
+        $user = $this->security->getUser();
+
+        return $this->objectMapper->map($user, UserMe::class);
     }
 }
